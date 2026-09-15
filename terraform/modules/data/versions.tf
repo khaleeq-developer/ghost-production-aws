32d82b2eec9fd664d9392d55594c20f75e5534d0
khaleeq-developer <khaleeq.developer@gmail.com>
2026-09-15 13:14:10 +0500

    tflint and versions applied across modules

---
+++ terraform/modules/data/versions.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_version = ">= 1.15.8"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 6.62"
+    }
+
+    random = {
+      source  = "hashicorp/random"
+      version = ">= 3.7"
+    }
+  }
+}
